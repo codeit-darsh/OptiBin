c2f1a7477591815ebb8cd6fc7e49de141d05a492
codeit-darsh <tyagiadarsh221@gmail.com>
2026-04-22 08:33:39 +0530

    Prepare for Render deploy: dynamic PORT, add .gitignore, remove db from tracking

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const path = require('path');
 const { Bins, Trucks, Reports, Notifications } = require('./database');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
